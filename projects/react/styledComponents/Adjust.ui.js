6a05b4e94c9577da68971f340206b550e166bed4
Alena1508 <alena.makarova1508@gmail.com>
2019-07-12 14:56:28 +0300

    Add dist

--- projects/react/styledComponents/Adjust.ui.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-
-const AdjustWrapper = styled.div`
-  color: ${props => props.theme.colors.text.base};
-  text-align: center;
-`;
-
-export { AdjustWrapper };
